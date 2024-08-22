0283289ec85e1f3c27c67fbed2673ce928479bcf
Dayron Barbosa <166651864+DdayronBarbosa@users.noreply.github.com>
2024-08-21 22:08:00 -0300

    Add files via upload

---
+++ Prova Algoritmos e Variáveis/ProvaAlgoritmosVariáveis.py
@@ -0,0 +1,31 @@
+inteiro  =  42
+
+ponto_flutuante  =  3.14
+
+texto  =  "Olá, Mundo!"
+
+booleano  =  True
+
+
+
+# Solicitação de dados ao usuário
+
+nome  =  input ( "Digite seu nome: " )
+
+
+
+# Imprima a solicitação anterior na tela com mensagem personalizada
+
+print ( f"Olá, { nome } !" )
+
+
+
+# Exemplo de uso das variáveis
+
+print ( f"Um número inteiro: { int } " )
+
+print ( f"Um número de ponto flutuante: { ponto_flutuante } " )
+
+print ( f"Uma string: { texto } " )
+
+print ( f"Uma variável booleana: { booleano } " )
